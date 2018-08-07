145b8ec5a4d16af5ac93b4327bb1f3ae662586ef
= <=>
2018-08-07 11:12:12 -0400

    it's now using the same format that is in place in some of our other applications - should be just fine.

--- local_settings.py
+++ local_settings.py
@@ -4,9 +4,15 @@ HEALTHCHECK_URL = "http://healthchecks/ping/303e6190-ac7d-4b94-ad5e-2e4db8e7732d
 LOG_NAME = ""
 LOG_PATH = ""
 #FILE_AGE = 60*60*48; # TIME IN SECONDS
-FILE_AGE = 20;
-FILE_PATHS = [".","nightly_file_management_"];
-FILE_PATTERNS = [".txt",".log"];
+FILE_AGE = (60*60*24*2); # shouldn't pick anything up.
+FILE_PATHS = ["nightly_file_management_","C:/Local_Code/repos/GEICS/sent_files"];
+FILE_PATTERNS = [".txt","_"];
+# PRUNE SETTINGS
+# this can be set to purge sent_files as well, just be careful that you don't delete anything you don't need. (all will be maintained in their source directories too.
+MAX_LOG_AGE = (7*60*60*24); # default to a week.
+PRUNE_PATTERNS = [".log",".interim","_log.txt"];   # truncate the log and counter files- just to prevent unecessary clutter - if you keep the counts you can see exactly how many were actually sent.
+PRUNE_PATHS = [".",".\\logs"];          # default to no paths.
+
 
 # need to change this up - we'll set up a new 
 #PATH_AGES={# key is going to be as follows: ("path","pattern"):age_in_seconds
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 """
 
 alrighty - going to change up how it's calling the mechanism, it doesn't seem to want to line up correctly.
+
+guess I'll just crossreference the version on the wsautomate...
 """
 
 import subprocess;
@@ -28,8 +30,7 @@ def main(args):
     #for a in PATH_AGES.keys():
     #    prune(PATH_AGES[a],a[1],a[0]);
     for a in FILE_PATHS:
-        for b in FILE_PATTERNS:
-            prune(FILE_AGE,b,a); 31
+        prune(FILE_AGE,FILE_PATTERNS,"",scan_paths=FILE_PATHS);
     # that's really all it needs to do.
     request.urlopen(HEALTHCHECK_URL);  
     
